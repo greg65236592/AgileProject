01d2c836e58002fa1698999e2efc09b4faecdffd
BB <101525006@cc.ncu.edu.tw>
2013-04-22 20:24:05 +0800

    modified TestshowFinishMsg.java

--- AgileProject/UnitTest/TestshowFinishMsg.java
+++ AgileProject/UnitTest/TestshowFinishMsg.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,11 +16,18 @@ public class TestshowFinishMsg
 		testUI = new UI();
 	}
 
+	
 	@Test
+	//Test Case 1：input： null expected output：print "結束了"
 	public void testshowFinishMsg()
 	{
+		String expected ="結束了";
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		
+		System.setOut(new PrintStream(outContent));
 		testUI.showFinishMsg();
-		assertEquals(true, true);
+		String[] result=outContent.toString().split("\r");
+		assertEquals(expected, result[0]);
 	}
 
 }
